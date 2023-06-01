bf8bcd5de73eedf0db5b849840f694dbab25d692
Maarten Hoogendoorn <maarten@moretea.nl>
2023-06-01 12:58:14 +0200

    allow not installed nix

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ else
   sh <(curl -L https://nixos.org/nix/install) --daemon --yes
 fi
 
-nix build --extra-experimental-features 'nix-command flakes' .#nixosConfigurations.kexec_installer.config.system.build.kexec_tarball 
+bash -c "nix build --extra-experimental-features 'nix-command flakes' .#nixosConfigurations.kexec_installer.config.system.build.kexec_tarball"
 
