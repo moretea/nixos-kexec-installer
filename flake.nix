0ef0ddca9e54338fceed9c2846781f642eb7a452
Maarten Hoogendoorn <maarten@moretea.nl>
2023-06-01 13:00:42 +0200

    blup

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   outputs = { self, nixpkgs }: {
-    nixosConfigurations.x86_64_linux.kexec_installer = let
+    nixosConfigurations.x86_64-linux.kexec_installer = let
       pkgs = import nixpkgs { system = "x86_64"; };
       configuration = pkgs.callPackage ./nix/configuration.nix;
     in configuration;
