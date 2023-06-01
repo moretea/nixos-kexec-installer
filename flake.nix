087ecfa302cdc6c79ba4dbcad3484733575391b1
Maarten Hoogendoorn <maarten@moretea.nl>
2023-06-01 13:10:12 +0200

    blup

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   outputs = { self, nixpkgs }: {
     nixosConfigurations.kexec_installer = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialAttrs = { inherit nixpkgs; };
+      specialArgs = { inherit nixpkgs; };
       modules = [ ./nix/configuration.nix ];
     };
   };
