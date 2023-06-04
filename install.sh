21c5ee9eecb96b9cff71083213f720bcfc626729
Maarten Hoogendoorn <maarten@moretea.nl>
2023-06-04 14:21:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,8 @@ if command -v nix > /dev/null; then
   echo "Nix already installed"
 else
   sh <(curl -L https://nixos.org/nix/install) --daemon --yes
+  . /etc/bash.bashrc # Load nix
 fi
-
-# Load nix
-. /etc/bash.bashrc
 
 nix build --extra-experimental-features 'nix-command flakes' .#nixosConfigurations.kexec_installer.config.system.build.kexec_tarball
 tar -xf ./result/tarball/nixos-system-x86_64-linux.tar.xz
