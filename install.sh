9ab1db8bac1bf7834a908c669c69cb278b56582c
Maarten Hoogendoorn <maarten@moretea.nl>
2023-06-04 14:15:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,9 @@ else
   sh <(curl -L https://nixos.org/nix/install) --daemon --yes
 fi
 
-bash -c "nix build --extra-experimental-features 'nix-command flakes' .#nixosConfigurations.kexec_installer.config.system.build.kexec_tarball"
+# Load nix
+. /etc/bash.bashrc
+
+nix build --extra-experimental-features 'nix-command flakes' .#nixosConfigurations.kexec_installer.config.system.build.kexec_tarball
 tar -xf ./result/tarball/nixos-system-x86_64-linux.tar.xz
 ./kexec_nixos
-
