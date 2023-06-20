b83d781a81498938e5f0d2033b3b7c131c744e45
Maarten Hoogendoorn <maarten@moretea.nl>
2023-06-20 17:47:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@ set -euo pipefail
 if command -v nix > /dev/null; then
   echo "Nix already installed"
 else
-  sh <(curl -L https://nixos.org/nix/install) --daemon --yes
-  . /etc/bash.bashrc # Load nix
+  echo "Install nix, by running: sh <(curl -L https://nixos.org/nix/install) --daemon --yes"
+  echo "Then re-run your shell via EXEC \$SHELL"
+  exit 1
 fi
 
 nix build --extra-experimental-features 'nix-command flakes' .#nixosConfigurations.kexec_installer.config.system.build.kexec_tarball
